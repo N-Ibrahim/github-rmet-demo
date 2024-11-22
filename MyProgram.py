8941e4e7ca9241d1f337cf1482efaff5e165894a
N-Ibrahim <52959966+N-Ibrahim@users.noreply.github.com>
2024-11-22 16:37:16 +0000

    Update MyProgram.py

--- MyProgram.py
+++ MyProgram.py
@@ -6,7 +6,7 @@ def subtract(first_number, second_number):
     return first_number - second_number
 
 def multiply(first_number, second_number):
-    return first_number * second_number
+    return first_number - second_number
 
 def divide(first_number, second_number):
     return first_number / second_number
